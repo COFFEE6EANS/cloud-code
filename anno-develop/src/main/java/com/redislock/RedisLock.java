3da0c5960819459dcf782e02aa0d4dd3cc85c1da
coffee6eans <coffee6eans@163.com>
2019-03-12 10:47:37 +0800

    tj

---
+++ anno-develop/src/main/java/com/redislock/RedisLock.java
@@ -0,0 +1,15 @@
+package com.redislock;
+
+import redis.clients.jedis.JedisPool;
+
+/**
+ * @Author guojianfeng.
+ * @Date Created in  2019/3/1
+ * @Description：
+ */
+public class RedisLock {
+
+    private JedisPool jedisPool;
+
+
+}
